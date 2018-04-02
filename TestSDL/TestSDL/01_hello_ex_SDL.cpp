de1a1cd52c7738d86c81b5f3697c520f1833040a
Nizar Kury <nkury@asu.edu>
2018-04-02 11:44:32 -0700

    Initial push

---
+++ TestSDL/TestSDL/01_hello_ex_SDL.cpp
@@ -0,0 +1,20 @@
+// Using SDL and standard IO
+#include <SDL.h>
+#include <stdio.h>
+
+//Screen dimension constraints
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* args[]) {
+	// The window we will be rendering to
+	SDL_Window* window = NULL;
+
+	// The surface contained by the window
+	SDL_Surface* screenSurface = NULL;
+
+	// Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+	}
+}
